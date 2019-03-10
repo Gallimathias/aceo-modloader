c2a60c24fe419b9aa0e155d4aa6afa9345c7e7af
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2019-03-10 15:57:48 +0100

    Modifie default busmod example to new logic

---
+++ Samples/BusNamesMod/BusModelPatch.cs
@@ -0,0 +1,34 @@
+﻿using Aceo.Sdk;
+using Aceo.Sdk.Patching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusNamesMod
+{
+    public class BusModelPatch : Patch
+    {
+        public override string TargetType => "BusModel";
+
+        [Override]
+        private readonly string[] availTypes = new string[]
+        {
+            "Volvo",
+            "Skania",
+            "VanHool",
+            "MAN",
+            "Alexander Dennis"
+        };
+
+        [Override]
+        private readonly string[] availCompanies = new string[]
+        {
+            "CapitalConnect",
+            "AirBus",
+            "Enterprise Rental Car Shuttle",
+            "Lot A Shuttle",
+            "Lot B Shuttle"
+        };
+    }
+}
